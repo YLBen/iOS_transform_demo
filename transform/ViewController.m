0f8b88c3e7a9104fd965e1e0174993233fbe2842
ben <1294353939@qq.com>
2018-08-15 10:23:25 +0800

    transform

--- transform/ViewController.m
+++ transform/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *btn;
 
 @end
 
@@ -16,14 +17,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setDefaultAnchorPointforView:self.btn];
+    [NSTimer scheduledTimerWithTimeInterval:0.5 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        [UIView animateWithDuration:2.5 animations:^{
+            self.btn.transform = CGAffineTransformRotate(self.btn.transform, M_PI);
+//            self.btn.transform = CGAffineTransformScale(self.btn.transform, 1.1, 1.1);
+        }];
+    }];
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)rotate {
+    [UIView animateWithDuration:2.5 animations:^{
+        self.btn.transform = CGAffineTransformRotate(self.btn.transform, M_PI);
+        self.btn.transform = CGAffineTransformScale(self.btn.transform, 1.1, 1.1);
+    }];
+    
+   
+    
 }
 
+- (IBAction)scale {
+    self.btn.transform = CGAffineTransformScale(self.btn.transform, 1.5, 1.5);
+}
+
+- (IBAction)goback {
+    self.btn.transform = CGAffineTransformIdentity;
+}
+
+- (IBAction)move {
+    self.btn.transform = CGAffineTransformTranslate(self.btn.transform, 10, 10);
+//    self.btn.transform = CGAffineTransformMakeTranslation(10, 10);
+}
+
+- (void)setAnchorPoint:(CGPoint)anchorPoint forView:(UIView *)view {
+    
+    CGPoint oldOrigin = view.frame.origin;
+    view.layer.anchorPoint = anchorPoint;
+    CGPoint newOrigin = view.frame.origin;
+    
+    CGPoint transition;
+    transition.x = newOrigin.x - oldOrigin.x;
+    transition.y = newOrigin.y - oldOrigin.y;
+    
+    view.center = CGPointMake (view.center.x - transition.x, view.center.y - transition.y);
+}
+
+- (void)setDefaultAnchorPointforView:(UIView *)view {
+    
+    [self setAnchorPoint:CGPointMake(0, 0) forView:view];
+}
 
 @end
