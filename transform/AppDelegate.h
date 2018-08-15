abdc3cd3a0e813bc8bd2bac10f4dded24c4ba80b
ben <1294353939@qq.com>
2018-08-15 09:49:26 +0800

    Initial Commit

---
+++ transform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  transform
+//
+//  Created by Ben Lv on 2018/8/15.
+//  Copyright © 2018年 avatar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ transform/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  transform
+//
+//  Created by Ben Lv on 2018/8/15.
+//  Copyright © 2018年 avatar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
